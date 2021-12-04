a9e694002abba1726eeaa57664bab0f4a03187f6
Aira Estur <92619533+airAwit@users.noreply.github.com>
2021-12-05 00:42:46 +0800

    Update ASSIGNMENT#8.1.py

--- ASSIGNMENT#8.1.py
+++ ASSIGNMENT#8.1.py
@@ -1,24 +1,28 @@
 #Lottery
 import random
-ticket = []
-lottery = []
 a = True
 while a == True:
+    ticket = []
+    lottery = []
+    
     #Get the user's lucky numbers
     for i in range (3):
         user_ticket = int (input ("Put your lucky number: "))
         ticket.append (user_ticket)
     print ("This is your ticket ", ticket)
+    
     #Generate random numbers
     for i in range (3):
         winning_number = random.randint (0,9)
         lottery.append (winning_number)
     print ("The winning numbers are ", lottery)
+    
     #Match numbers in ticket and in lottery
     if ticket in lottery:
         print ("You Win, Congratulations")
     else:
         print ("You Lose, Please try again")
+        
     #Ask the player to play again
     another_ticket = input ("Do you want to try again? (type 'y' if yes, and 'n' if no): ")
     another_ticket.lower
